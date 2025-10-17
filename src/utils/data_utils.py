98abda1f9c273c861e8e99b38eeadbeeca111333
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-10-17 04:00:33 +0300

    Add column names to load_data return values for better data handling

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -11,5 +11,6 @@ def preprocessing(data, standardize: bool = False):
 def load_data():
     df = pd.read_csv("data\dataset_200x4_regression.csv", sep=',')
     x, t = df.iloc[:, :3], df.iloc[:, 3]
+    cols_names = list(x.columns)
     x = preprocessing(x)
-    return x, t
+    return x, t, cols_names
